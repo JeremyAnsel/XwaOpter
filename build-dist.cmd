4702b60e1cdd62414e401a7e6d5d87f17ce30b68
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-06-02 19:39:32 +0200

    Fix build-dist.cmd

--- build-dist.cmd
+++ build-dist.cmd
@@ -8,6 +8,7 @@ xcopy dist\ bld\dist\ /E
 For %%a in (
 "XwaOpter\XwaOpter\bin\Release\net48\*.exe"
 "XwaOpter\XwaOpter\bin\Release\net48\*.exe.config"
+"XwaOpter\XwaOpter\bin\Release\net48\*.dll"
 ) do (
 xcopy /s /d "%%~a" bld\dist\
 )
